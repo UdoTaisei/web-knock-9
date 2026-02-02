c2d1d2a0a1becad79c9009a215ef038b95ee5171
Udo Taisei <taiqingyoudong@gmail.com>
2026-02-02 21:05:13 +0900

    kadai94

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello, Render kadai93!");
+  res.send(`Hello, Render ${process.env.kadai_number || "varError"}!`);
 });
 
 app.listen(PORT, () => {
